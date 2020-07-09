88dca910fcb8f01c33e585b276c28d4406313cd5
Hamza Iqbal <hiqbal.bese16seecs@seecs.edu.pk>
2020-07-10 00:12:19 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
