c7e53b8c2798905fc35ef55ff630f737635f4b80
Hamza Iqbal <hiqbal.bese16seecs@seecs.edu.pk>
2020-07-24 22:59:06 +0500

    Updates

---
+++ precache-manifest.76b76b5af3fd713303e2ad4fa48ec5b9.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafc9b4601c40b2d296f29c4bca69172",
+    "url": "/party-live/index.html"
+  },
+  {
+    "revision": "b7acb88b6a78ecc0533b",
+    "url": "/party-live/static/css/2.c32e6bef.chunk.css"
+  },
+  {
+    "revision": "f8c42843c9f9b7437993",
+    "url": "/party-live/static/css/main.002ecfcc.chunk.css"
+  },
+  {
+    "revision": "b7acb88b6a78ecc0533b",
+    "url": "/party-live/static/js/2.76ab6230.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/party-live/static/js/2.76ab6230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c42843c9f9b7437993",
+    "url": "/party-live/static/js/main.b61bcd9b.chunk.js"
+  },
+  {
+    "revision": "7f6b78bff4d3baeaf962",
+    "url": "/party-live/static/js/runtime-main.d5df50dd.js"
+  },
+  {
+    "revision": "0eda847353bcb7e9f5b762626b1597d1",
+    "url": "/party-live/static/media/1stdancer.0eda8473.jpeg"
+  },
+  {
+    "revision": "6abbd4a4a86c24a367894820add6de64",
+    "url": "/party-live/static/media/2nddancer.6abbd4a4.jpg"
+  },
+  {
+    "revision": "7031b9e38fcdc1336891b4219ca25512",
+    "url": "/party-live/static/media/Akkordeon-DEMO.7031b9e3.otf"
+  },
+  {
+    "revision": "58adf240b0778eb2f05938cce2bcba64",
+    "url": "/party-live/static/media/backShow.58adf240.png"
+  },
+  {
+    "revision": "eea5ba4d18e1dfff28e70608437a1883",
+    "url": "/party-live/static/media/bg1.eea5ba4d.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/party-live/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/party-live/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/party-live/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/party-live/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/party-live/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/party-live/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "3ddc8f08ba3149a5f371aabe2cbad4bd",
+    "url": "/party-live/static/media/friends.3ddc8f08.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/party-live/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/party-live/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/party-live/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/party-live/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/party-live/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "05b4f37fa2647c286529be8176408db3",
+    "url": "/party-live/static/media/logo.05b4f37f.png"
+  },
+  {
+    "revision": "9aff6d8347f8e0321d24d71114b33a00",
+    "url": "/party-live/static/media/message.9aff6d83.png"
+  },
+  {
+    "revision": "69560be566a01ba97efbaf2af76ea306",
+    "url": "/party-live/static/media/nextDancer.69560be5.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/party-live/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/party-live/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/party-live/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/party-live/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/party-live/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "4835803e56ddac28a0fa5aa59606d55a",
+    "url": "/party-live/static/media/ranking.4835803e.png"
+  },
+  {
+    "revision": "55b5f2b01cee6fd4cdac6ec76bf60652",
+    "url": "/party-live/static/media/you.55b5f2b0.png"
+  }
+]);
